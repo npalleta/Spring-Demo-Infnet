69d4cdbc9092b5f49cad151c98749a37d4dd9328
npalleta <palleta.sp@gmail.com>
2023-04-02 20:08:05 -0300

    Indentation Adjustment #2

--- src/main/java/br/com/infnet/lessons/spring/SpringDemo/controller/NoteController.java
+++ src/main/java/br/com/infnet/lessons/spring/SpringDemo/controller/NoteController.java
@@ -29,7 +29,6 @@ public class NoteController {
 
     @GetMapping("/notes/{id}")
     public Note getNoteById(@PathVariable(value = "id") Long noteId) {
-
         return noteRepository
                 .findById(noteId)
                 .orElseThrow(() -> new ResourceNotFoundException("Note", "id", noteId));
@@ -37,7 +36,6 @@ public class NoteController {
 
     @PutMapping("/notes/{id}")
     public Note updateNote(@PathVariable(value = "id") Long noteId, @Valid @RequestBody Note noteDetails) {
-
         Note note = noteRepository
                 .findById(noteId)
                 .orElseThrow(() -> new ResourceNotFoundException("Note", "id", noteId));
@@ -48,7 +46,6 @@ public class NoteController {
 
     @DeleteMapping("/notes/{id}")
     public ResponseEntity<?> deleteNote(@PathVariable(value = "id") Long noteId) {
-
         Note note = noteRepository
                 .findById(noteId)
                 .orElseThrow(() -> new ResourceNotFoundException("Note", "id", noteId));
