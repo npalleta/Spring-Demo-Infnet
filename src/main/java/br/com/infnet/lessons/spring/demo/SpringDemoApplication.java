a05bece1670868f3c6f5aee9f93f2349b24d8537
npalleta <palleta.sp@gmail.com>
2023-04-09 17:36:28 -0300

    slf4j included

--- src/main/java/br/com/infnet/lessons/spring/demo/SpringDemoApplication.java
+++ src/main/java/br/com/infnet/lessons/spring/demo/SpringDemoApplication.java
@@ -1,5 +1,7 @@
 package br.com.infnet.lessons.spring.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -8,7 +10,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 public class SpringDemoApplication {
 
+	private static final Logger LOG = LoggerFactory.getLogger(SpringDemoApplication.class);
+
 	public static void main(String[] args) {
+		LOG.info("INFO: LOGGING...");
 		SpringApplication.run(SpringDemoApplication.class, args);
 	}
 }
